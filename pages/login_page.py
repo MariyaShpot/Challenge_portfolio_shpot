dbba0b56cf3bf8b0547024e7feb48c57f6ca8d33
MariyaShpot <mariya.roksolyana.shpot@gmail.com>
2023-01-23 00:52:40 +0200

    Add Task 2: Subtask 2

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,10 +1,14 @@
-# from pages.base_page import BasePage
-#
-#
-# class LoginPage(BasePage):
-#     login_field_xpath = "//*[@id='login']"
-#     password_field_xpath =
-#     sign_in_button_xpath =
-#
-#     def type_in_email(self, email):
-#         self.field_send_keys(self.login_field_xpath, email)
+from pages.base_page import BasePage
+
+
+class LoginPage(BasePage):
+    login_field_xpath = "//*[@id='login']"
+    password_field_xpath = "//*[@id='password']"
+    sign_in_button_xpath = "//*[@type='submit']"
+    scouts_panel_title_xpath = "//*[text()='Scouts Panel']"
+    remind_password_hyperlink_xpath = "//*[contains(@tabindex, -1)]"
+    dropdown_button_xpath = "//*[text()='English' and @role='button']"
+
+
+    def type_in_email(self, email):
+        self.field_send_keys(self.login_field_xpath, email)
