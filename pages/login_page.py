38d8cca687e99036baa66975cb3a6d858a4e6a7b
MariyaShpot <mariya.roksolyana.shpot@gmail.com>
2023-01-29 14:36:19 +0200

    Add Task 2: Subtask 2: Correction

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,9 +4,9 @@ from pages.base_page import BasePage
 class LoginPage(BasePage):
     login_field_xpath = "//*[@id='login']"
     password_field_xpath = "//*[@id='password']"
-    sign_in_button_xpath = "//*[@type='submit']"
+    sign_in_button_xpath = "//button[@type='submit']"
     scouts_panel_title_xpath = "//*[text()='Scouts Panel']"
-    remind_password_hyperlink_xpath = "//*[contains(@tabindex, -1)]"
+    remind_password_hyperlink_xpath = "//div/div[1]/a"
     dropdown_button_xpath = "//*[text()='English' and @role='button']"
 
 
