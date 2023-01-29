88e0bb76be78ad94fa0884aded1cb72dbdd72572
MariyaShpot <mariya.roksolyana.shpot@gmail.com>
2023-01-29 19:51:17 +0200

    Add Task 3: Subtask 2

---
+++ test_cases/login_to_the_system.py
@@ -0,0 +1,31 @@
+import os
+import time
+import unittest
+from selenium import webdriver
+
+from pages.login_page import LoginPage
+from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
+
+
+class TestLoginPage(unittest.TestCase):
+
+    driver = None
+
+    @classmethod
+    def setUp(self):
+        os.chmod(DRIVER_PATH, 755)
+        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+        self.driver.get('https://scouts-test.futbolkolektyw.pl/en')
+        self.driver.fullscreen_window()
+        self.driver.implicitly_wait(IMPLICITLY_WAIT)
+
+    def test_login_to_the_system(self):
+        user_login = LoginPage(self.driver)
+        user_login.type_in_email('user08@getnada.com')
+        user_login.fill_in_password('Test-1234')
+        user_login.click_on_the_sign_in_button()
+        time.sleep(5)
+
+    @classmethod
+    def tearDown(self):
+        self.driver.quit()
