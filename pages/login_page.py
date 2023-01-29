7dc6d99d1248ba4eaac4dd73da363e86f7ac757c
MariyaShpot <mariya.roksolyana.shpot@gmail.com>
2023-01-29 18:14:51 +0200

    Add Task 3: Subtask 1 (+additional methods)

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,11 +10,18 @@ class LoginPage(BasePage):
     dropdown_button_xpath = "//*[text()='English' and @role='button']"
 
 
+
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
 
     def fill_in_password(self, password):
         self.field_send_keys(self.password_field_xpath, password)
 
-    def click_the_button(self, click):
-        self.click_on_the_element(self.sign_in_button_xpath, click)
+    def click_on_the_sign_in_button(self):
+        self.click_on_the_element(self.sign_in_button_xpath)
+
+    def click_on_the_remind_password_hyperlink(self):
+        self.click_on_the_element(self.remind_password_hyperlink_xpath)
+
+    def click_on_the_dropdown_button(self):
+        self.click_on_the_element(self.dropdown_button_xpath)
