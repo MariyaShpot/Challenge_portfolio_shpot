9b61fd11d425d17d35f94037f5a2bbef995d07ca
MariyaShpot <mariya.roksolyana.shpot@gmail.com>
2023-02-01 00:21:16 +0200

    Add Task 3: Subtask 4(Correction)

--- test_cases/add_player_tc.py
+++ test_cases/add_player_tc.py
@@ -1,6 +1,7 @@
 from _pytest import unittest
 from selenium.webdriver.chrome import webdriver
 
+from pages.add_a_player import click_on_the_add_player_button
 from pages.base_page import BasePage
 from pages.dashboard import Dashboard
 from pages.login_page import LoginPage
@@ -25,7 +26,7 @@ class TestAddAPlayer(unittest.TestCase):
         self.driver.fullscreen_window()
         self.driver.implicitly_wait(IMPLICITLY_WAIT)
 
-    def test_login_to_the_system(self, dashboard_page=None, add_a_player_page=None):
+    def test_login_to_the_system(self, dashboard_page='Dashboard', add_a_player_page='add_player_url'):
         user_login = LoginPage(self.driver)
         user_login.type_in_email('user08@getnada.com')
         user_login.fill_in_password('Test-1234')
@@ -33,7 +34,7 @@ class TestAddAPlayer(unittest.TestCase):
         time.sleep(5)
         user_login.dashboard_page = Dashboard(self.driver)
         time.sleep(5)
-        dashboard_page.click_on_the_add_player_button()
+        click_on_the_add_player_button(self)
         time.sleep(5)
         add_a_player_page.title_of_page()
 
